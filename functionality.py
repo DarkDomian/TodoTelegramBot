544536235fde4e5ecae2fa4c928d510203dc156d
DarkDomian <darkdomian@gmail.com>
2022-07-10 16:26:59 +0300

    version 1

--- functionality.py
+++ functionality.py
@@ -1,6 +1,33 @@
-# functionality
-todolist = {}
+help = """
+help - display a list of available commands and ther function
+add - add a task to the list
+show -  display all tasks
+exit - stop the program"""
 
-date = input('When? ')
-task = input('What? ')
-todolist[date] = [task]
+todolist = {
+  "today": [],
+  "tomorrow": [],
+  "other": {}
+}
+
+while True:
+  command = input("Write a command: ")
+  if command == "help":
+    print(help)
+  elif command == "show":
+    print(todolist)
+  elif command == "add":
+    date = input("Wrate the date: ")
+    task = input("Write a task: ")
+    if date == 'today':
+      todolist['today'].append(task)
+    elif date == 'tomorrow':
+      todolist['tomorrow'].append(task)
+    else:
+      todolist['other'] = dict(date=task)
+      # have some trouble
+  elif command == "exit":
+    break
+  else:
+    print("there is no such command, but we are working on it")
+  
